4c2926f35571193edeb0ab765827e1181e8e12e9
Luis Diego <lfdiego7@gmail.com>
2019-10-25 17:48:52 -0500

    se agrega tags

--- test/test.develop.js
+++ test/test.develop.js
@@ -2,9 +2,13 @@ import http from "k6/http";
 import { check } from "k6";
 
 export default function() {
-  let res = http.get(`http://34.221.230.9:3004`);
+  let res = http.get(`http://34.221.230.9:3004`, { tags: { env: "develop" } });
 
-  check(res, {
-    "status was 200": r => r.status == 200
-  });
+  check(
+    res,
+    {
+      "status was 200": r => r.status == 200
+    },
+    { env: "develop" }
+  );
 }
--- test/test.release.js
+++ test/test.release.js
@@ -2,9 +2,13 @@ import http from "k6/http";
 import { check } from "k6";
 
 export default function() {
-  let res = http.get(`http://52.27.92.223:3004`);
+  let res = http.get(`http://52.27.92.223:3004`, { tags: { env: "release" } });
 
-  check(res, {
-    "status was 200": r => r.status == 200
-  });
+  check(
+    res,
+    {
+      "status was 200": r => r.status == 200
+    },
+    { env: "release" }
+  );
 }
